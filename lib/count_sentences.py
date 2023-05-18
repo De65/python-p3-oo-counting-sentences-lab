84c02ba58e9cebb9ecd24f29254a7c5837a88f0e
De65 <kyalodiana6@gmail.com>
2023-05-18 12:54:04 +0300

    completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,50 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, v=""):
+        self._value = v
+    
+    def get_value(self):
+        return self._value
+    
+    def set_value(self, value):
+        if type(value) == str:
+            self._value = value
+        else:
+            print("The value must be a string.")
+    
+    def is_sentence(self):
+        try:
+            if self._value.index("."):
+                return True
+            else:
+                return False
+        except:
+            return False
+        
+    def is_question(self):
+        try:
+            if self._value.index("?"):
+                return True
+            else:
+                return False
+        except:
+            return False
+    
+    def is_exclamation(self):
+        try:
+            if self._value.index("!"):
+                return True
+            else:
+                return False
+        except:
+            return False
+        
+    def count_sentences(self):
+        count = 0
+        spl = self._value.split(" ")
+        for el in spl:
+            if el.__contains__('.') or el.__contains__('!') or el.__contains__('?'):
+                count+=1
+        return count 
+    value = property(get_value,set_value)
